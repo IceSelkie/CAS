fab0146139bcbf5acbca654b3989bfe8ccaeca3b
StanleyMines <stanleymines@gmail.com>
2018-11-22 00:40:34 -0800

    Started javadocs/comments. Little reorganization for javadocs/comments.

--- Int.java
+++ Int.java
@@ -6,118 +6,234 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import static com.hypereclipse.selkie.casbot.util.BotStatic.ats;
 import static com.hypereclipse.selkie.casbot.util.BotStatic.atsr;
 
+
+
+/**
+ * class Int (extends Value)
+ *
+ * Represents an integer value in a little-endian array of hexadecimal values.
+ * Lowest level representation of a value.
+ *
+ * All basic math operations can be applied.
+ *
+ * @author Selkie (Stanley S.)
+ * @version 1.0
+ * @since 1.0
+ */
 public class Int implements Value
 {
+  /** A public constant representing the commonly used value of -1. */
   public static final Int NEGATIVEONE = new Int(-1);
+  /** A public constant representing the commonly used value of 0. */
   public static final Int ZERO = new Int(0);
+  /** A public constant representing the commonly used value of 1. */
   public static final Int ONE = new Int(1);
+  /** A public constant representing the commonly used value of 2. */
   public static final Int TWO = new Int(2);
+  /** A public constant representing the value 10. */
+  public static final Int TEN = new Int(10);
+  /** A public constant representing the commonly used value of 16. */
   public static final Int SIXTEEN = new Int(16);
+  /** A public constant representing the maximum value that an integer can hold. */
   public static final Int INTMIN = new Int(Integer.MIN_VALUE);
+  /** A public constant representing the minimum value that an integer can hold. */
   public static final Int INTMAX = new Int(Integer.MAX_VALUE);
+  /** A public constant representing the maximum value that a long can hold. */
   public static final Int LONGMIN = new Int(Long.MIN_VALUE);
+  /** A public constant representing the minimum value that a long can hold. */
   public static final Int LONGMAX = new Int(Long.MAX_VALUE);
 
 
-  // SmallEndian Hex Int
-  boolean negative;
-  int[] value; //Hex: 0-F
+  /** <code>true</code> if negative. <code>false</code> otherwise. */
+  private boolean negative;
+  /**
+   * Represents the infinite precision integer in a little-endian array of hexadecimal values.
+   * Value at the 0th index is the 0x1s place, and values in the 1st index are the 0x10s place, etc.
+   * Note: It MAY be padded with zeros. See: {@link com.hypereclipse.selkie.cas.Int#isClean}.
+   */
+  private int[] value; //Hex: 0-F
+  /**
+   * Represents if the {@link com.hypereclipse.selkie.cas.Int#value} is "clean".
+   * <code>true</code> asserts that the array has no extraneous tailing zeros.
+   * Ex: 0x0010 would be <code>false</code>.
+   * Ex: 0x10 should be <code>true</code>, but may not be if {@link com.hypereclipse.selkie.cas.Int#clean()} has not been called.
+   */
+  private boolean isClean = false;
+  /**
+   * A hashmap to store products once they are calculated so they don't need to be recalculated, since multiplication is decently expensive.
+   */
+  private static HashMap<Int, HashMap<Int,Int>> precalculatedMultiplications = new HashMap<>();
+  // Should one be added for division/modulus? Probably.
 
+
+
+  /**
+   * Constructor for {@link com.hypereclipse.selkie.cas.Int}.
+   *
+   * Creates a new {@link com.hypereclipse.selkie.cas.Int} object from the given primitive value.
+   *
+   * @param value The input integer primitive to convert to an {@link com.hypereclipse.selkie.cas.Int}.
+   * @since 1.0
+   */
   public Int(long value)
   {
-    this.value = new int[((Long) value).toString().length()];
+    // Deal with negatives
     if (value < 0)
     {
       negative = true;
       value = -value;
     }
-    int i = 0;
-    while (value > 0)
+
+    // Determine the length of the array by using the number of digits in the number in base ten.
+    this.value = new int[((Long) value).toString().length()];
+
+    // Fill in the digits in the value array.
+    for (int i = 0; value > 0; i++)
     {
-      this.value[i++] = (int) (value % 0x10);
+      this.value[i] = (int) (value % 0x10);
       value /= 0x10;
     }
   }
 
+  /**
+   * Private constructor for {@link com.hypereclipse.selkie.cas.Int}.
+   *
+   * This constructor is used internally by {@link com.hypereclipse.selkie.cas.Int} to create new {@link com.hypereclipse.selkie.cas.Int} objects given an existing set of instance variables.
+   * Requires that the {@link int[]} will never be modified after the object is created, and thus is private.
+   *
+   * @param value The existing array of digits for a new {@link com.hypereclipse.selkie.cas.Int} object.
+   * @param negative Whether the new {@link com.hypereclipse.selkie.cas.Int} is negative or not.
+   * @since 1.0
+   */
   private Int(int[] value, boolean negative)
   {
     this.value = value;
     this.negative = negative;
   }
 
+
+
+  /**
+   * Adds a {@link com.hypereclipse.selkie.cas.Value} to this {@link com.hypereclipse.selkie.cas.Int}.
+   *
+   * Since {@link com.hypereclipse.selkie.cas.Int} is the lowest level representation of a value, if the <code>other</code> value is anything other than an {@link com.hypereclipse.selkie.cas.Int}, it's {@link com.hypereclipse.selkie.cas.Value#add(Value)} will be called instead.
+   *
+   * If the <code>other</code> value is an {@link com.hypereclipse.selkie.cas.Int}, it will add them together and create a new {@link com.hypereclipse.selkie.cas.Int} to represent them.
+   *
+   * @param other The other value to be added to this one.
+   * @return A new {@link com.hypereclipse.selkie.cas.Value} representing the added value. If the <code>other</code> value is an {@link com.hypereclipse.selkie.cas.Int}, then it will return a new {@link com.hypereclipse.selkie.cas.Int} object with the added value.
+   * @since 1.0
+   */
   public Value add(Value other)
   {
-    if (other instanceof Int)
-    {
-      Int o = (Int) other;
-
-      if (o.isZero()) return this;
-      if (isZero()) return o;
-      if (o.isNegative()) return subtract(o.negate());
-      if (isNegative()) return o.subtract(this.negate());
-
-      int lenMax = value.length < o.value.length ? o.value.length + 1 : value.length + 1;
-      int[] ret = new int[lenMax];
-      boolean carry = false;
-      for (int i = 0; i < lenMax; i++)
-      {
-        ret[i] = get(i) + o.get(i) + (carry ? 1 : 0);
-        if (ret[i] > 0xF)
-        {
-          ret[i] -= 0x10;
-          carry = true;
-        }
-        else
-          carry = false;
-      }
-      return new Int(ret, false);
-    }
-    else
+    // We only want to process it if we know how to (when its also an Int). Otherwise, we'll have their class process it.
+    if (!(other instanceof Int))
       return other.add(this);
+
+    Int o = (Int)other;
+
+    // Simple cases that require no processing
+    if (o.isZero()) return this;
+    if (isZero()) return o;
+    // Special cases that require some processing before we can begin.
+    if (o.isNegative()) return subtract(o.negate());
+    if (isNegative()) return o.subtract(this.negate());
+
+    // Determine the new Int's length. The longer length + 1 should be safe. (Incase of a carry)
+    int lenMax = value.length < o.value.length ? o.value.length + 1 : value.length + 1;
+    int[] ret = new int[lenMax];
+
+    // Boolean to hold if there is a sixteen to carry.
+    boolean carry = false;
+
+    // For each digit, ...
+    for (int i = 0; i < lenMax; i++)
+    {
+      // ... add each value together (and add one if carry), ...
+      ret[i] = get(i) + o.get(i) + (carry ? 1 : 0);
+
+      // ... and if its too large, carry.
+      carry = false;
+      if (ret[i] > 0xF)
+      {
+        ret[i] -= 0x10;
+        carry = true;
+
+        if (ret[i]>0xF)
+          throw new IllegalStateException("Carrying a number does not solve out of bounds error. See Int#add(Int). " + atsr(this.value) + " " + atsr(o.value));
+      }
+    }
+    // If there is another carry we haven't dealt with... Then something is broken and it needs to be fixed.
+    if (carry)
+      throw new IllegalStateException("Carrying a number beyond its bound. See Int#add(Int). " + atsr(this.value) + " " + atsr(o.value));
+    return new Int(ret, false);
   }
 
+  /**
+   * Subtracts a {@link com.hypereclipse.selkie.cas.Value} from this {@link com.hypereclipse.selkie.cas.Int}.
+   *
+   * Since {@link com.hypereclipse.selkie.cas.Int} is the lowest level representation of a value, if the <code>other</code> value is anything other than an {@link com.hypereclipse.selkie.cas.Int}, it's {@link com.hypereclipse.selkie.cas.Value#subtract(Value)} will be called instead.
+   *
+   * If the <code>other</code> value is an {@link com.hypereclipse.selkie.cas.Int}, it will subtract it from this and create a new {@link com.hypereclipse.selkie.cas.Int} to represent them.
+   *
+   * @param other The other value to be subtracted from this one.
+   * @return A new {@link com.hypereclipse.selkie.cas.Value} representing the subtracted value. If the <code>other</code> value is an {@link com.hypereclipse.selkie.cas.Int}, then it will return a new {@link com.hypereclipse.selkie.cas.Int} object with the subtracted value.
+   * @since 1.0
+   */
   public Value subtract(Value other)
   {
-    if (other instanceof Int)
+    // We only want to process it if we know how to (when its also an Int). Otherwise, we'll have their class process it.
+    if (!(other instanceof Int))
+      other.negate().add(this);
+
+    Int o = (Int)other;
+
+    // Simple cases that require no processing
+    if (o.isZero()) return this;
+    if (isZero()) return o.negate();
+    if (equals(o)) return ZERO;
+    // Special cases that require some processing before we can begin.
+    if (o.isNegative()) return add(o.negate());
+    if (isNegative()) return o.negate().add(this.negate());
+
+    // We want our answer that we generate here to be positive, so should the number we are subtracting be larger, flip it so that cant happen.
+    if (this.less(o))
+      return o.subtract(this).negate();
+
+    // ASSERTION: <code>this</code> must be greater than <code>o</code> now.
+
+    // The new Int's length will be at most this Int's length, since this is greater than o. (Not adding, so the new value's magnitude wont be larger than the largest existing one)
+    int lenMax = value.length;
+    int[] ret = new int[lenMax];
+
+    // The value being carried up (cuts out of the next digit)
+    // should the digit being subtracted be larger than what its being subtracted from, that's a problem since our {@link #value} only holds positive values. We must carry "take" a 0x10 out of the next digit, so we can stay positive. This is our "carry".
+    boolean carry = false;
+
+    // For each digit, ...
+    for (int i = 0; i < lenMax; i++)
     {
-      Int o = (Int) other;
+      // ... subtract the values (and a few more if needed by the carry), ...
+      ret[i] = get(i) - o.get(i) - (carry ? 1 : 0);
 
-      if (o.isZero()) return this;
-      if (isZero()) return o.negate();
-      if (equals(o)) return new Int(new int[0], false);
-      if (o.isNegative()) return add(o.negate());
-      if (isNegative()) return o.add(this.negate());
-
-      if (this.less(o))
-        return o.subtract(this).negate();
-
-      // This must be greater than o now.
-
-      int lenMax = value.length < o.value.length ? o.value.length + 1 : value.length + 1;
-      int[] ret = new int[lenMax];
-      int carry = 0;
-
-      for (int i = 0; i < lenMax; i++)
+      // ... and if its too small add 0x10 so its not and carry that to the next digit.
+      carry = false;
+      if (ret[i] < 0x0)
       {
-        ret[i] = get(i) - o.get(i) - carry;
-        carry = 0;
-        while (ret[i] < 0x0)
-        {
-          ret[i] += 0x10;
-          carry++;
-        }
+        ret[i] += 0x10;
+        carry = true;
+        if (ret[i]<0x0)
+          throw new IllegalStateException("Carrying a negative number does not solve negative. See Int#subtract(Int). " + atsr(this.value) + " " + atsr(o.value));
       }
-      if (carry > 0)
-        throw new IllegalArgumentException("Carrying a negative number beyond its bound. See Int#subtract(Int). "+atsr(this.value)+" "+atsr(o.value));
-      return new Int(ret, false);
     }
-    return other.add(this);
+    // If there is another carry we haven't dealt with... Then something is broken and it needs to be fixed.
+    if (carry)
+      throw new IllegalStateException("Carrying a negative number beyond its bound. See Int#subtract(Int). " + atsr(this.value) + " " + atsr(o.value));
+    return new Int(ret, false);
   }
 
-  HashMap<Int, HashMap<Int,Int>> precalculated = new HashMap<>();
   public Value multiply(Value other)
   {
     if (other instanceof Int)
@@ -127,8 +243,8 @@ public class Int implements Value
       clean();
       o.clean();
 
-      if (precalculated.containsKey(this) && precalculated.get(this).containsKey(o))
-        return precalculated.get(this).get(o);
+      if (precalculatedMultiplications.containsKey(this) && precalculatedMultiplications.get(this).containsKey(o))
+        return precalculatedMultiplications.get(this).get(o);
 
       if (o.value.length < value.length)
         return o.multiply(this);
@@ -143,9 +259,9 @@ public class Int implements Value
       if (isNegative()^o.isNegative())
         ret = (Int)ret.negate();
 
-      if (!precalculated.containsKey(this))
-        precalculated.put(this,new HashMap<>());
-      precalculated.get(this).put(o,ret);
+      if (!precalculatedMultiplications.containsKey(this))
+        precalculatedMultiplications.put(this,new HashMap<>());
+      precalculatedMultiplications.get(this).put(o,ret);
 
       return ret;
     }
@@ -427,8 +543,6 @@ public class Int implements Value
     if (index < 0 || index >= value.length) return 0;
     return value[index];
   }
-
-  private boolean isClean = false;
 
   private void clean()
   {
